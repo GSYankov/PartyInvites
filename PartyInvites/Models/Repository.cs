7196117f8d88011b0e2484a42cb838129ce86e7b
Georgi Yankov <spookyfox@abv.bg>
2018-06-20 16:14:33 +0300

    Initial upload

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+
+        }
+
+        public static void AddG()
+        {
+            if (responses.Count==0)
+            {
+                GuestResponse g = new GuestResponse { Name = "Georgi Yankov",
+                                                      Email = "Georgi.S.Yankov@gmail.com",
+                                                      Phone = "+359 887 101040",
+                                                      WillAttend = true };
+                responses.Add(g);
+            }
+        }
+    }
+}
